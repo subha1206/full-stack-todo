c5632483da5aa23915adb277d231060e99a6c53f
Subha Sarkar <sarkarsubha1206@gmail.com>
2020-07-24 11:47:42 +0530

    final update

--- public/browser.js
+++ public/browser.js
@@ -1,4 +1,57 @@
+const submitBtn = document.querySelector(".btn-primary");
+
+const itemTemplate = (item) => {
+  return ` <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+  <span class="item-text">${item.text}</span>
+  <div>
+    <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+    <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+  </div>
+</li>`;
+};
+
+let listTemplate = items.map((item) => {
+  return itemTemplate(item);
+}).join('')
+document
+  .getElementById("item-list")
+  .insertAdjacentHTML("beforeend", listTemplate);
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const userInput = document.querySelector("#input").value;
+  if (userInput) {
+    axios
+      .post("/create-item", {
+        text: userInput,
+      })
+      .then((res) => {
+        document
+          .getElementById("item-list")
+          .insertAdjacentHTML("beforeend", itemTemplate(res.data));
+        document.querySelector("#input").value = "";
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+});
+
 document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("Do you want to delete the item ?")) {
+      axios
+        .post("/delete-item", {
+          id: e.target.getAttribute("data-id"),
+        })
+        .then((res) => {
+          e.target.parentElement.parentElement.remove();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+
   if (e.target.classList.contains("edit-me")) {
     let userEdit = prompt(
       "Enter your text here...",
@@ -15,7 +68,7 @@ document.addEventListener("click", (e) => {
           e.target.parentElement.parentElement.querySelector(
             ".item-text"
           ).innerHTML = userEdit;
-          console.log(res)
+          console.log(res);
         })
         .catch((err) => {
           console.log(err);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongodb = require("mongodb");
+const sanitizeHTML = require("sanitize-html");
 
 const app = express();
 let db;
@@ -38,31 +39,22 @@ app.get("/", (req, res) => {
     <h1 class="display-4 text-center py-1">To-Do App</h1>
     
     <div class="jumbotron p-3 shadow-sm">
-      <form action="/create-item" method="POST">
+      <form>
         <div class="d-flex align-items-center">
-          <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+          <input name="item" id="input" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
           <button class="btn btn-primary">Add New Item</button>
         </div>
       </form>
     </div>
     
-    <ul class="list-group pb-5">
-    ${items
-      .map((item) => {
-        return `
-      <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-      <span class="item-text">${item.text}</span>
-      <div>
-        <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-        <button class="delete-me btn btn-danger btn-sm">Delete</button>
-      </div>
-    </li>
-      `;
-      })
-      .join("")}
+    <ul id="item-list" class="list-group pb-5">
+   
     </ul>
     
   </div>
+  <script>
+  let items = ${JSON.stringify(items)}
+  </script>
   <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
   <script src="/browser.js"></script>
 </body>
@@ -72,18 +64,35 @@ app.get("/", (req, res) => {
 });
 
 app.post("/create-item", (req, res) => {
-  db.collection("items").insertOne({ text: req.body.item }, () => {
-    res.redirect("/");
+  let safeText = sanitizeHTML(req.body.text, {
+    allowedTags: [],
+    allowedAttributes: {},
+  });
+
+  db.collection("items").insertOne({ text: safeText }, (err, info) => {
+    res.json(info.ops[0]);
   });
 });
 
 app.post("/update-item", (req, res) => {
-  
+  let safeText = sanitizeHTML(req.body.text, {
+    allowedTags: [],
+    allowedAttributes: {},
+  });
   db.collection("items").findOneAndUpdate(
-    {_id: new mongodb.ObjectId(req.body.id)},
-    { $set: { text: req.body.text } },
+    { _id: new mongodb.ObjectId(req.body.id) },
+    { $set: { text: safeText } },
     () => {
       res.send("Success");
+    }
+  );
+});
+
+app.post("/delete-item", (req, res) => {
+  db.collection("items").deleteOne(
+    { _id: new mongodb.ObjectId(req.body.id) },
+    () => {
+      res.send("Deleted succesfully");
     }
   );
 });
